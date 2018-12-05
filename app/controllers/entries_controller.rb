e782ed10f70677cc307fbdba902db1a729a3ac4a
msynko <marina.synko@gmail.com>
2018-12-05 17:21:53 -0500

    Views and Instance Variables

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,53 @@
+class EntriesController < ApplicationController
+
+def index
+  @entries = Entry.all
+end
+
+def show
+   @entry = Entry.find(params[:id])
+end
+
+def new
+  @entry = Entry.new
+end
+
+def create
+      @entry = Entry.new
+      @entry.word = params[:entry][:word]
+      @entry.definition = params[:entry][:definition]
+      @entry.language = params[:entry][:language]
+  if @entry.save
+      redirect_to entries_path
+  else
+      render :new
+  end
+end
+
+
+def edit
+  @entry = Entry.find(params[:id])
+end
+
+def update
+   @entry = Entry.find(params[:id])
+
+   @entry.word = params[:entry][:word]
+   @entry.definition = params[:entry][:definition]
+   @entry.language = params[:entry][:language]
+
+   if @entry.save
+     redirect_to entries_path
+   else
+     render :edit
+   end
+ end
+
+
+ def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
+  end
+
+end
